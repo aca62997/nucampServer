a44f5fd9d213e01ff68fbc9551bbb5f0300ab723
Amanda Abrams <aca62997@gmail.com>
2022-03-16 11:20:50 -0400

    HTTPS

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -165,7 +165,7 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
     res.statusCode = 403;
     res.end(`POST operation not supported on /campsites/${req.params.campsiteId}/comments/${req.params.commentId}`);
 })
-.put(authenticate.verifyUser, (req, res, next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
@@ -224,5 +224,4 @@ campsiteRouter.route('/:campsiteId/comments/:commentId')
     })
     .catch(err => next(err));
 });
-​
 module.exports = campsiteRouter;
